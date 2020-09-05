e42333d84f96d224b288fe2b131c7e9331d460c4
yerimah <idee1140@gmail.com>
2020-09-05 11:05:22 +0100

    project_setup

---
+++ demo_utils/src/main/java/com/eghoindustries/demo_utils/DemoUtil.kt
@@ -0,0 +1,13 @@
+package com.eghoindustries.demo_utils
+
+/**
+ *Created by Yerimah on 9/5/2020.
+ */
+class DemoUtil{
+
+    var TAG = "DDLS  => "
+
+    private fun log(message: String) {
+        println(TAG + message)
+    } 
+}
